21b175eab92eb61948bad26567cfe272b94867b0
Juhyung Park <qkrwngud825@gmail.com>
2022-05-22 04:37:03 +0900

    SF: Scheduler: opportunistically enter idle after touch
    This commit drastically changes how the scheduler works.
    
    Now, the device will be forced to maintain idle state (set to 60 Hz in
    this commit) after touchboost is finished and idle state is entered once.
    
    The main principle of this commit is that the highest refresh rate isn't
    necessary for everything and applying it only for touch-related events
    are plenty enough.
    
    It's been deliberately set to 60 Hz so that anomalies from the nature of
    imperfectiveness of this method is minimum and less-noticeable compared
    to the lowest possible refresh rate.
    
    Example of this commit in action:
    
    <Before 1>
    | Touch starts | Scrolling starts | Touch ends | Scrolling ends | Scroll-bar timeout | Scroll-bar fade-out animation | Idle  |
    | 120 Hz       | 120 Hz           | 120 Hz     | 120 Hz         | 24 Hz              | 120 Hz                        | 24 Hz |
    
    <After 1>
    | Touch starts | Scrolling starts | Touch ends | Scrolling ends | Scroll-bar timeout | Scroll-bar fade-out animation | Idle  |
    | 120 Hz       | 120 Hz           | 120 Hz     | 120 Hz         | 24 Hz              | 60 Hz                         | 24 Hz |
    
    <Before 2>
    | Idle  | Wi-Fi status bar icon changes | Timeout | Idle  |
    | 24 Hz | 120 Hz                        | 120 Hz  | 24 Hz |
    
    <After 2>
    | Idle  | Wi-Fi status bar icon changes | Timeout | Idle  |
    | 24 Hz | 60 Hz                         | 60 Hz   | 24 Hz |
    
    [ghostrider-reborn: adapt to 13]
    
    Change-Id: I27dc60f35821ee4a5726f67f9b538b394194d00c
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- services/surfaceflinger/Scheduler/RefreshRateConfigs.cpp
+++ services/surfaceflinger/Scheduler/RefreshRateConfigs.cpp
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-// #define LOG_NDEBUG 0
+#define LOG_NDEBUG 0
 #define ATRACE_TAG ATRACE_TAG_GRAPHICS
 
 // TODO(b/129481165): remove the #pragma below and fix conversion issues
@@ -312,6 +312,8 @@ auto RefreshRateConfigs::getBestRefreshRateLocked(const std::vector<LayerRequire
     float maxExplicitWeight = 0;
     int seamedFocusedLayers = 0;
 
+    static bool localIsIdle;
+
     for (const auto& layer : layers) {
         switch (layer.vote) {
             case LayerVoteType::NoVote:
@@ -362,6 +364,7 @@ auto RefreshRateConfigs::getBestRefreshRateLocked(const std::vector<LayerRequire
     if (signals.touch) {
         const DisplayModePtr& max = getMaxRefreshRateByPolicyLocked(anchorGroup);
         ALOGV("TouchBoost - choose %s", to_string(max->getFps()).c_str());
+        localIsIdle = false;
         return {max, GlobalSignals{.touch = true}};
     }
 
@@ -374,6 +377,7 @@ auto RefreshRateConfigs::getBestRefreshRateLocked(const std::vector<LayerRequire
     if (!signals.touch && signals.idle && !(primaryRangeIsSingleRate && hasExplicitVoteLayers)) {
         const DisplayModePtr& min = getMinRefreshRateByPolicyLocked();
         ALOGV("Idle - choose %s", to_string(min->getFps()).c_str());
+        localIsIdle = true;
         return {min, GlobalSignals{.idle = true}};
     }
 
@@ -466,6 +470,20 @@ auto RefreshRateConfigs::getBestRefreshRateLocked(const std::vector<LayerRequire
             ? getMaxScoreRefreshRate(scores.rbegin(), scores.rend())
             : getMaxScoreRefreshRate(scores.begin(), scores.end());
 
+    const auto selectivelyForceIdle = [&] () -> std::pair<DisplayModePtr, GlobalSignals>  {
+        ALOGV("localIsIdle: %s", localIsIdle ? "true" : "false");
+        if (localIsIdle && isStrictlyLess(60_Hz, bestRefreshRate->getFps())) {
+            /*
+             * We heavily rely on touch to boost higher than 60 fps.
+             * Fallback to 60 fps if an higher fps was calculated.
+             */
+            ALOGV("Forcing idle");
+            return {mIdleRefreshRate, kNoSignals};
+        }
+
+        return {bestRefreshRate, kNoSignals};
+    };
+
     if (primaryRangeIsSingleRate) {
         // If we never scored any layers, then choose the rate from the primary
         // range instead of picking a random score from the app range.
@@ -475,7 +493,7 @@ auto RefreshRateConfigs::getBestRefreshRateLocked(const std::vector<LayerRequire
             ALOGV("layers not scored - choose %s", to_string(max->getFps()).c_str());
             return {max, kNoSignals};
         } else {
-            return {bestRefreshRate, kNoSignals};
+            return selectivelyForceIdle();
         }
     }
 
@@ -503,7 +521,7 @@ auto RefreshRateConfigs::getBestRefreshRateLocked(const std::vector<LayerRequire
         return {touchRefreshRate, GlobalSignals{.touch = true}};
     }
 
-    return {bestRefreshRate, kNoSignals};
+    return selectivelyForceIdle();
 }
 
 std::unordered_map<uid_t, std::vector<const RefreshRateConfigs::LayerRequirement*>>
@@ -835,6 +853,10 @@ void RefreshRateConfigs::constructAvailableRefreshRates() {
             for (const auto modeIt : modes) {
                 str += to_string(modeIt->second->getFps());
                 str.push_back(' ');
+                if (isApproxEqual(modeIt->second->getFps(), 60_Hz)) {
+                    mIdleRefreshRate = modeIt->second;
+                    ALOGV("idleRefreshRate set!");
+                }
             }
             return str;
         };
